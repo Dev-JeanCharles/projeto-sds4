b92f2f20f50998eb694bdad0449688ee9bea2466
Elliot85 <jeancduasan@gmail.com>
2021-09-06 23:36:16 -0300

    Footer change

--- frontend/src/components/navbar/Footer/index.tsx
+++ frontend/src/components/navbar/Footer/index.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <div><footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/Elliot85" target="_blank" rel="noreferrer">Jean Charles</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/JeanCharles14" target="_blank" rel="noreferrer">Jean Charles</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
                     Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
